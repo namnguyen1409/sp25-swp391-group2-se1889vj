ecc6cc0a2a823f2fac647f81d19dc876093d7bc0
Nguyen Van Nam <namnvhe180863@fpt.edu.vn>
2025-02-03 09:31:42 +0700

    completed database

--- src/main/java/com/group2/sp25swp391group2se1889vj/customer/entity/Customer.java
+++ src/main/java/com/group2/sp25swp391group2se1889vj/customer/entity/Customer.java
@@ -1,4 +1,41 @@
 package com.group2.sp25swp391group2se1889vj.customer.entity;
 
+import com.group2.sp25swp391group2se1889vj.invoice.entity.Invoice;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false, columnDefinition = "nvarchar(255)")
+    private String fullName;
+
+    @Column(name = "phone", nullable = true, columnDefinition = "nvarchar(20)")
+    private String phone;
+
+    @Column(name = "email", nullable = true, columnDefinition = "nvarchar(255)")
+    private String email;
+
+    @Column(name = "address", nullable = true, columnDefinition = "nvarchar(255)")
+    private String address;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Invoice> invoices = new HashSet<>();
+
 }
---
+++ src/main/java/com/group2/sp25swp391group2se1889vj/invoice/enums/InvoiceType.java
@@ -0,0 +1,6 @@
+package com.group2.sp25swp391group2se1889vj.invoice.enums;
+
+public enum InvoiceType {
+    PAYMENT,
+    DEBT
+}
