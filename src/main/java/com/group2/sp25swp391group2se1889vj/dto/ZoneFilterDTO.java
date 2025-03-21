d37cf58d66bf1720a7cacbef05dd632860f37d55
Nguyen Van Nam <namnvhe180863@fpt.edu.vn>
2025-03-21 08:22:43 +0700

    update zone

--- src/main/java/com/group2/sp25swp391group2se1889vj/dto/ZoneFilterDTO.java
+++ src/main/java/com/group2/sp25swp391group2se1889vj/dto/ZoneFilterDTO.java
@@ -2,6 +2,9 @@ package com.group2.sp25swp391group2se1889vj.dto;
 
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Builder
@@ -11,4 +14,8 @@ import lombok.*;
 public class ZoneFilterDTO extends BaseFilterDTO {
     private String name;
     private String productName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime minUpdateAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime maxUpdateAt;
 }
